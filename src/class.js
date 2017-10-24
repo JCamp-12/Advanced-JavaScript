05d4a3faeb9e766bae52050949b0a9460cc202f7
JCamp-12 <username@users.noreply.github.com>
2017-10-24 10:56:27 -0400

    classes work, cat extends animal

--- src/class.js
+++ src/class.js
@@ -6,6 +6,16 @@
 // for a potential password that will be compared to the `password` property.
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(pw) {
+    if (pw === this.password) return true;
+    return false;
+  }
+}
 
 /* eslint-disable no-undef */ // Remove this comment once you write your classes.
 
@@ -14,11 +24,29 @@
 // `Cat` should extend the `Animal` class.
 // Animal and Cat should both have a parameter called `options` in their constructors.
 // Animal should have the property `age` that's set in the constructor and the method
-// `growOlder` that returns the age.
+// `growOlder` that returns the age. + a year.
 // Cat should have the property `name` that is set in the constructor and the method
 // `meow` that should return the string `<name> meowed!` where `<name>` is the `name`
 // property set on the Cat instance.
 
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+  growOlder() {
+    return this.age + 1;
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
 
 module.exports = {
   User,
